cd9a2df41678c1a2790bbc6825baa65cce0c7bbf
Agustí <agusti@lacalafa.cat>
2022-01-22 16:42:50 +0100

    Primer

---
+++ primer.js
@@ -0,0 +1,57 @@
+public class registre()
+  {
+    int tipus_registre;
+
+    public registre()
+    {
+
+    }
+    public registre Crear11()
+    {
+
+    }
+
+
+
+
+
+public class fitxer()
+  {
+    int numero_registres;
+    registre registres[];
+
+    public bool nouFitxer(string cami);
+    {
+      return bool;
+      int numero_registres 0 =,
+      if(registres.crear11()      )
+      {
+
+      }
+    }
+
+    public bool afegirRegistre(string data1, string data2, string moviment, string masdatos, long import, long saldo)
+    {
+      registre nou = registre();
+      nou.afegirData1(string data1);
+      nou.afegirData2(string data2);
+      registres.Add(nou);
+      return true;
+    }
+
+    public bool Acabarfitxer();
+    {
+      registres.registres.crear
+    }
+
+    public bool guardar(string cami)
+    {
+      for (i=0; i < registres.retornaTotal; i++)
+      {
+        registres.escriualFitxer(cami, i);
+      }
+      return true;
+    }
+    public void escriualFitxer(string cami, int numero)
+  }
+}
